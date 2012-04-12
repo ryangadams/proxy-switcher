083ecacfe1b19e55c5a8ab61dcddc516f98888c7
Ryan Adams <ryan.g.adams@gmail.com>
2012-04-12 22:28:55 +0100

    curl and stunner support

--- services.rb
+++ services.rb
@@ -35,8 +35,32 @@ module Services
     puts 'run "source ~/.bash_profile" to update environment settings'
   end
   def Services.git(on_or_off)
+    puts "toggling gitconfig settings - #{on_or_off}"
+    gitconfig = "#{Dir.home}/.gitconfig"
+    proxy_set = "proxy"
+    Services.make_the_change(gitconfig, proxy_set, "# #{proxy_set}", on_or_off)
+  end
+  def Services.curl(on_or_off)
+    puts "toggling curl settings - #{on_or_off}"
+    curlrc = "#{Dir.home}/.curlrc"
+    proxy_set = "--proxy"
+    Services.make_the_change(curlrc, proxy_set, "# #{proxy_set}", on_or_off) 
   end
   def Services.stunnel(on_or_off)
+    puts "toggling stunnel settings - #{on_or_off}"
+    `killall stunnel`
+    stunnel_conf = "/usr/local/etc/stunnel/stunnel.conf"
+    line_one_off = "sslVersion = SSLv3"
+    line_two_off = "connect"
+    line_one_on = "sslVersion = TLSv1"
+    line_two_on = "exec"
+    line_three_on = "execargs"
+    Services.make_the_change(stunnel_conf, "; #{line_one_off}", line_one_off, on_or_off)
+    Services.make_the_change(stunnel_conf, "; #{line_two_off}", line_two_off, on_or_off)
+    Services.make_the_change(stunnel_conf, line_one_on, "; #{line_one_on}", on_or_off)
+    Services.make_the_change(stunnel_conf, line_two_on, "; #{line_two_on}", on_or_off)
+    Services.make_the_change(stunnel_conf, line_three_on, "; #{line_three_on}", on_or_off)
+    `stunnel`
   end
   def Services.location(on_or_off)
   end
@@ -47,6 +71,7 @@ module Services
     Services.bash(on_or_off)
     Services.subversion(on_or_off)
     Services.git(on_or_off)
+    Services.curl(on_or_off)
     Services.stunnel(on_or_off)
     Services.location(on_or_off)
   end
